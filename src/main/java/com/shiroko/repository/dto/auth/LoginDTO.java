3d3cda3d429157c17d7ed459195c617f8268684a
KurimulaAiri <132827937+KurimulaAiri@users.noreply.github.com>
2026-04-29 19:09:57 +0800

    feat(auth): 添加登录和注册相关DTO及校验分组
    - 新增LoginDTO用于处理不同登录方式的参数校验
    - 新增RegisterDTO用于用户注册参数校验
    - 新增LoginGroup接口定义不同登录方式的校验分组
    - 删除不再使用的RecordVO类
    - 新增课程记录相关DTO（Insert/Update/Delete/Query）

---
+++ src/main/java/com/shiroko/repository/dto/auth/LoginDTO.java
@@ -0,0 +1,37 @@
+package com.shiroko.repository.dto.auth;
+
+import com.shiroko.repository.dto.auth.validategroup.LoginGroup;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Description: 登录DTO
+ *
+ * @author Guguguy
+ * @version 1.0
+ * @since 2026/3/19 下午4:10
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class LoginDTO {
+    @NotNull(message = "code不能为空", groups = LoginGroup.LoginNoPwd.class)
+    private String code;
+
+    @NotNull(message = "openId不能为空", groups = {LoginGroup.LoginByPwd.class, LoginGroup.LoginByToken.class})
+    private String openId;
+
+    @NotNull(message = "role不能为空", groups = LoginGroup.LoginByPwd.class)
+    private Long role;
+
+    @NotNull(message = "account不能为空", groups = LoginGroup.LoginByPwd.class)
+    private String account;
+
+    @NotNull(message = "password不能为空", groups = LoginGroup.LoginByPwd.class)
+    private String password;
+
+    @NotNull(message = "token不能为空", groups = {LoginGroup.LoginByToken.class, LoginGroup.LoginOut.class})
+    private String token;
+}
---
+++ src/main/java/com/shiroko/repository/dto/courserecord/QueryCourseRecordDTO.java
@@ -0,0 +1,36 @@
+package com.shiroko.repository.dto.courserecord;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Description: TODO
+ *
+ * @author Guguguy
+ * @version 1.0
+ * @since 2026/2/7 上午1:19
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class QueryCourseRecordDTO {
+
+    private boolean share;
+
+    private Long id;
+
+    private Long userId;
+
+    private String stuName;
+
+    private String courseName;
+
+    private String courseRemark;
+
+    private Long courseStatus;
+
+    private Long currentPage;
+
+    private Long pageSize;
+}
